8159ad076c5ce4dc80fe3a1857330289bc3404ae
hankyeol-jung <whd6023@gmail.com>
2022-11-07 15:34:10 +0900

    control bar 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faAngleRight,
+  faAngleLeft,
+  faAnglesRight,
+  faAnglesLeft,
+} from "@fortawesome/free-solid-svg-icons";
+
+let CircleBox = styled.button`
+  background: #ddd;
+  border-radius: 50%;
+  width: 34px;
+  height: 34px;
+  border: none;
+  color: #fff;
+  margin: 8px;
+`;
+let NormalBox = styled.button`
+  background: #ddd;
+  border-radius: 15px;
+  width: 90px;
+  height: 30px;
+  border: none;
+  color: #fff;
+  margin: 0 15px;
+`;
 
 function App() {
+  const keyboard = Array.from({ length: 31 }, (v, i) => i + 1);
+  let [pageNum, setPageNum] = useState(keyboard);
+  console.log(pageNum[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar pageNum={pageNum} />
+    </div>
+  );
+}
+
+function NavBar(props) {
+  return (
+    <div className="controlBar">
+      <CircleBox>
+        <FontAwesomeIcon icon={faAnglesLeft} />
+      </CircleBox>
+      <CircleBox>
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </CircleBox>
+      <NormalBox>
+        {props.pageNum[0]} / {props.pageNum.slice(-1)}
+      </NormalBox>
+      <CircleBox>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </CircleBox>
+      <CircleBox>
+        <FontAwesomeIcon icon={faAnglesRight} />
+      </CircleBox>
     </div>
   );
 }
