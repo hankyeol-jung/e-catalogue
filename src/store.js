0c4785987d0165916d4fd31acf2de0875d3d2eeb
hankyeol-jung <whd6023@gmail.com>
2022-11-15 10:05:51 +0900

    이미지 배치 수정

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
-import img1 from "./image/page1.jpg";
-import img2 from "./image/page2.jpg";
-import img3 from "./image/page3.jpg";
-import img4 from "./image/page4.jpg";
-import img5 from "./image/page5.jpg";
-import img6 from "./image/page6.jpg";
-import img7 from "./image/page7.jpg";
-import img8 from "./image/page8.jpg";
+import img1 from "./image/page5.jpg";
+import img2 from "./image/page6.jpg";
+import img3 from "./image/page7.jpg";
+import img4 from "./image/page8.jpg";
+import img7 from "./image/page3.jpg";
+import img8 from "./image/page4.jpg";
+import img5 from "./image/page1.jpg";
+import img6 from "./image/page2.jpg";
 
 // data or img 소스 받아오는 state
 let contents = createSlice({
